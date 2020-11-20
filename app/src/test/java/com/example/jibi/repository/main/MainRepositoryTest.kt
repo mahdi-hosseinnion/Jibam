a91299f20ca318511a484078f82c4854a3b33ab5
seyed mahdi <ssmmhh1382@gmail.com>
2020-11-20 10:01:48 +0330

    add exception to last commit

--- app/src/test/java/com/example/jibi/repository/main/MainRepositoryTest.kt
+++ app/src/test/java/com/example/jibi/repository/main/MainRepositoryTest.kt
@@ -1,14 +1,15 @@
 package com.example.jibi.repository.main
 
+import android.util.Log
 import com.example.jibi.persistence.CategoriesDao
 import com.example.jibi.persistence.RecordsDao
 import com.example.jibi.persistence.getSumOfIncome
 import com.example.jibi.util.DataState
-import io.mockk.MockKAnnotations
-import io.mockk.every
+import com.example.jibi.util.Response
+import com.example.jibi.util.ResponseType
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
-import io.mockk.unmockkAll
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
@@ -24,6 +25,8 @@ import org.junit.Before
 
 @ExperimentalCoroutinesApi
 class MainRepositoryTest {
+    private val ERROR_MESSAGE = "SOMETHING GOES WRUNG"
+
     //system under test
     lateinit var mainRepository: MainRepository
 
@@ -39,6 +42,9 @@ class MainRepositoryTest {
         mainRepository = MainRepository(
             recordsDao, categoriesDao
         )
+        //mock log.e for flow.catch in asDataState()
+        mockkStatic(Log::class)
+        every { Log.e(any(), any(), any()) } returns 0
     }
 
     @After
@@ -51,20 +57,24 @@ class MainRepositoryTest {
     fun getSumOfIncomeAndAsLiveData() = runBlocking {
         //setup
         every { recordsDao.returnTheSumOfAllIncome() } returns flow {
-            delay(1000)
+            delay(500)
             emit(50)
             delay(500)
             emit(56)
+            delay(500)
+            throw Exception(ERROR_MESSAGE)
         }
         val listOfResult = ArrayList<DataState<Int?>>()
         mainRepository.getSumOfIncome().collect {
             println(it.toString())
             listOfResult.add(it)
         }
+        //assert
         println("run assertes")
         assertEquals(listOfResult[0], DataState.loading<Int?>(isLoading = true))
         assertEquals(listOfResult[1].data?.data?.peekContent(), 50)
         assertEquals(listOfResult[2].data?.data?.peekContent(), 56)
+        assertEquals(listOfResult[3].error?.peekContent()?.response?.message, ERROR_MESSAGE)
     }
 
 }
