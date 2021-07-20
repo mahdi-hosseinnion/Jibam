a53e39a29314449360cd7bddaf91a652f49294ec
seyed mahdi <ssmmhh1382@gmail.com>
2021-07-20 09:45:43 +0430

    add new slide to appIntro and change app launcher logo

--- app/src/main/java/com/example/jibi/ui/app_intro/AppIntroActivity.kt
+++ app/src/main/java/com/example/jibi/ui/app_intro/AppIntroActivity.kt
@@ -4,10 +4,12 @@ import android.content.SharedPreferences
 import android.content.res.Resources
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.example.jibi.BaseApplication
 import com.example.jibi.R
+import com.example.jibi.util.Constants
 import com.example.jibi.util.PreferenceKeys
 import com.github.appintro.AppIntro
 import com.github.appintro.AppIntroFragment
@@ -25,12 +27,16 @@ class AppIntroActivity : AppIntro() {
     lateinit var sharedPrefsEditor: SharedPreferences.Editor
 
     @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    @Inject
     lateinit var _resources: Resources
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         inject()
         super.onCreate(savedInstanceState)
+        checkForRtlUi()
         // Call addSlide passing your Fragments.
         // You can use AppIntroFragment to use a pre-built fragment
         this.setSkipText(_getString(R.string.skip))
@@ -38,10 +44,18 @@ class AppIntroActivity : AppIntro() {
 
         addSlide(
             AppIntroFragment.newInstance(
+                title = _getString(R.string.app_intro_slide_0_title),
+                description = _getString(R.string.app_intro_slide_0_description),
+                imageDrawable = R.drawable.ic_app_logo_svg,
+                backgroundDrawable = R.drawable.app_intro_back_slide_5,
+            )
+        )
+        addSlide(
+            AppIntroFragment.newInstance(
                 title = _getString(R.string.app_intro_slide_1_title),
                 description = _getString(R.string.app_intro_slide_1_description),
-                imageDrawable = R.drawable.ic_app_intro_search,
-                backgroundDrawable = R.drawable.app_intro_back_slide_1,
+                imageDrawable = R.drawable.ic_app_intro_transaction_list,
+                backgroundDrawable = R.drawable.app_intro_back_slide_3,
             )
         )
         addSlide(
@@ -52,6 +66,22 @@ class AppIntroActivity : AppIntro() {
                 backgroundDrawable = R.drawable.app_intro_back_slide_2,
             )
         )
+    }
+
+    private fun checkForRtlUi() {
+        val appLanguage = sharedPreferences.getString(
+            PreferenceKeys.APP_LANGUAGE_PREF,
+            Constants.APP_DEFAULT_LANGUAGE
+        )
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            if (appLanguage == Constants.PERSIAN_LANG_CODE) {
+                window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL
+            }
+            if (appLanguage == Constants.ENGLISH_LANG_CODE) {
+                window.decorView.layoutDirection = View.LAYOUT_DIRECTION_LTR
+
+            }
+        }
     }
 
     private fun inject() {
