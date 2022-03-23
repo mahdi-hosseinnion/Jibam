b081db5e5de1c44b7b63e339454602985168f63b
mahdi <ssmmhh1382@gmail.com>
2022-03-23 14:49:04 +0430

    remove auto migration b/c its completely unnecessary

--- app/src/main/java/com/ssmmhh/jibam/persistence/AppDatabase.kt
+++ app/src/main/java/com/ssmmhh/jibam/persistence/AppDatabase.kt
@@ -16,9 +16,6 @@ import com.ssmmhh.jibam.persistence.entities.TransactionEntity
         CategoryImageEntity::class
     ],
     version = 5,
-    autoMigrations = [
-        AutoMigration(from = 4, to = 5)]
-    ,
 )
 abstract class AppDatabase : RoomDatabase() {
 
