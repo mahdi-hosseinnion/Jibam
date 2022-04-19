ac4331fe89e15324085db41d0e973df84bf67a59
mahdi <ssmmhh1382@gmail.com>
2022-04-19 19:07:14 +0430

    setup basic databinding in setting fragment

--- app/src/main/java/com/ssmmhh/jibam/presentation/setting/SettingFragment.kt
+++ app/src/main/java/com/ssmmhh/jibam/presentation/setting/SettingFragment.kt
@@ -7,12 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.ssmmhh.jibam.R
+import com.ssmmhh.jibam.databinding.FragmentAboutUsBinding
 import com.ssmmhh.jibam.databinding.FragmentSettingBinding
 import com.ssmmhh.jibam.presentation.common.BaseFragment
+import com.ssmmhh.jibam.presentation.util.ToolbarLayoutListener
 import com.ssmmhh.jibam.util.PreferenceKeys
 import com.ssmmhh.jibam.util.PreferenceKeys.APP_CALENDAR_PREFERENCE
 import com.ssmmhh.jibam.util.PreferenceKeys.CALENDAR_GREGORIAN
 import com.ssmmhh.jibam.util.PreferenceKeys.CALENDAR_SOLAR
+import com.ssmmhh.jibam.util.localizeNumber
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import java.util.*
@@ -25,40 +28,27 @@ class SettingFragment(
     private val currentLocale: Locale,
     private val sharedPreferences: SharedPreferences,
     private val sharedPrefEditor: SharedPreferences.Editor
-) : BaseFragment() {
-
-    private var _binding: FragmentSettingBinding? = null
-
-    private val binding get() = _binding!!
+) : BaseFragment(), ToolbarLayoutListener {
+    private lateinit var binding: FragmentSettingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentSettingBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        binding = FragmentSettingBinding.inflate(inflater, container, false).apply {
+            listener = this@SettingFragment
+        }
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.toolbar.topAppBarNormal.title = getString(R.string.setting)
         initUi()
     }
 
     private fun initUi() {
-        binding.toolbar.topAppBarNormal.setNavigationOnClickListener {
-            navigateBack()
-        }
-
         setDataToCalenderRadioGroup()
-
         binding.gregorianRb.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 sharedPrefEditor.putString(APP_CALENDAR_PREFERENCE, CALENDAR_GREGORIAN)
@@ -90,5 +80,11 @@ class SettingFragment(
     override fun handleStateMessages() {}
 
     override fun handleLoading() {}
+
+    override fun onClickOnNavigation(view: View) {
+        navigateBack()
+    }
+
+    override fun onClickOnMenuButton(view: View) {}
 
 }
