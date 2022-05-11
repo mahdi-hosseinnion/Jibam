eaf55910d1d00164ac8595f950d6f71d77abc17c
mahdi <ssmmhh1382@gmail.com>
2022-05-12 02:32:31 +0430

    refactor ChartListAdapter

--- app/src/main/java/com/ssmmhh/jibam/presentation/chart/chart/ChartListAdapter.kt
+++ app/src/main/java/com/ssmmhh/jibam/presentation/chart/chart/ChartListAdapter.kt
@@ -1,20 +1,12 @@
 package com.ssmmhh.jibam.presentation.chart.chart
 
-import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.RequestManager
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.ssmmhh.jibam.R
 import com.ssmmhh.jibam.data.model.ChartData
 import com.ssmmhh.jibam.databinding.LayoutChartListItemBinding
-import com.ssmmhh.jibam.util.localizeNumber
-import com.ssmmhh.jibam.util.separate3By3
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import java.util.*
 import kotlin.math.abs
 
 @ExperimentalCoroutinesApi
@@ -25,7 +17,27 @@ class ChartListAdapter(
 
     private var data = emptyList<ChartData>()
 
-    private var biggestPercentage: Float = 100.0f
+    private var biggestPercentage: Int = 100
+
+    class ChartViewHolder(
+        private val binding: LayoutChartListItemBinding,
+        private val viewModel: ChartViewModel,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: ChartData, biggestPercentage: Int) = with(binding) {
+            this.viewmodel = viewModel
+            this.item = item
+            this.biggestPercentage = biggestPercentage
+        }
+    }
+
+    fun submitList(list: List<ChartData>) {
+        if (!list.isNullOrEmpty()) {
+            biggestPercentage = list.maxOf { abs(it.percentage) }.toInt().coerceAtMost(100)
+        }
+        data = list
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 
@@ -49,27 +61,4 @@ class ChartListAdapter(
 
     override fun getItemCount(): Int = data.size
 
-    fun submitList(list: List<ChartData>) {
-        if (!list.isNullOrEmpty()) {
-            biggestPercentage = list.maxOf { abs(it.percentage) }
-        }
-        data = list
-        notifyDataSetChanged()
-    }
-
-    @ExperimentalCoroutinesApi
-    @FlowPreview
-    class ChartViewHolder
-    constructor(
-        private val binding: LayoutChartListItemBinding,
-        private val viewModel: ChartViewModel,
-    ) : RecyclerView.ViewHolder(binding.root) {
-
-
-        fun bind(item: ChartData, biggestPercentage: Float) = with(binding) {
-            this.viewmodel = viewModel
-            this.item = item
-            this.biggestPercentage = biggestPercentage.toInt()
-        }
-    }
 }
