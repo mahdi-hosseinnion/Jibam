e2bf357357fd716de4b33d2f9d499cd98685b93e
mahdi <ssmmhh1382@gmail.com>
2022-05-21 20:57:42 +0430

    fix AddHeaderToTransactions's isTheSameDayAs bug in different time zones

--- app/src/main/java/com/ssmmhh/jibam/presentation/transactions/AddHeaderToTransactions.kt
+++ app/src/main/java/com/ssmmhh/jibam/presentation/transactions/AddHeaderToTransactions.kt
@@ -1,58 +1,55 @@
 package com.ssmmhh.jibam.presentation.transactions
 
 import com.ssmmhh.jibam.data.source.local.dto.TransactionDto
-import com.ssmmhh.jibam.util.DateUtils
+import com.ssmmhh.jibam.util.DateUtils.toMilliSeconds
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.math.BigDecimal
+import java.util.*
 
-
-fun addHeaderToTransactions(currentList: List<TransactionDto>): List<TransactionsRecyclerViewItem> {
-    if (currentList.isEmpty()) {
-        return listOf(TransactionsRecyclerViewItem.DatabaseIsEmpty)
-    }
-    val resultList = ArrayList<TransactionsRecyclerViewItem>()
-    var headerDate = currentList[0].date
-    var incomeSum = BigDecimal.ZERO
-    var expensesSum = BigDecimal.ZERO
-    val tempList = ArrayList<TransactionsRecyclerViewItem>()
-    currentList.forEach { item ->
-        /**
-         * There is serious here.
-         * When there is differnt time zones like
-         * [1652716786,1652716772,1652643609,1652630378]
-         * These are all the same day in Iran day time but different day in GMT.
-         */
-        if (item.date.isTheSameDayAs(headerDate)) {
-            //make new header and items
-            tempList.add(item.toTransactionsRecyclerViewItem())
-            if (item.money >= BigDecimal.ZERO) { //income
-                incomeSum += item.money
-            } else { //expenses
-                expensesSum += item.money
-            }
-        } else {
-            //add header and items
-            resultList.add(createHeader(headerDate, incomeSum, expensesSum, tempList.size))
-            resultList.addAll(tempList)
-            //clear item to defualt
-            headerDate = item.date
-            tempList.clear()
-            incomeSum = BigDecimal.ZERO
-            expensesSum = BigDecimal.ZERO
-            //make new header and items
-            tempList.add(item.toTransactionsRecyclerViewItem())
-            if (item.money >= BigDecimal.ZERO) { //income
-                incomeSum += item.money
-            } else { //expenses
-                expensesSum += item.money
+suspend fun addHeaderToTransactions(currentList: List<TransactionDto>): List<TransactionsRecyclerViewItem> =
+    withContext(Dispatchers.IO) {
+        if (currentList.isEmpty()) {
+            return@withContext listOf(TransactionsRecyclerViewItem.DatabaseIsEmpty)
+        }
+        val resultList = ArrayList<TransactionsRecyclerViewItem>()
+        var headerDate = currentList[0].date
+        var incomeSum = BigDecimal.ZERO
+        var expensesSum = BigDecimal.ZERO
+        val tempList = ArrayList<TransactionsRecyclerViewItem>()
+        currentList.forEach { item ->
+            if (item.date.isTheSameDayAs(headerDate)) {
+                //make new header and items
+                tempList.add(item.toTransactionsRecyclerViewItem())
+                if (item.money >= BigDecimal.ZERO) { //income
+                    incomeSum += item.money
+                } else { //expenses
+                    expensesSum += item.money
+                }
+            } else {
+                //add header and items
+                resultList.add(createHeader(headerDate, incomeSum, expensesSum, tempList.size))
+                resultList.addAll(tempList)
+                //clear item to defualt
+                headerDate = item.date
+                tempList.clear()
+                incomeSum = BigDecimal.ZERO
+                expensesSum = BigDecimal.ZERO
+                //make new header and items
+                tempList.add(item.toTransactionsRecyclerViewItem())
+                if (item.money >= BigDecimal.ZERO) { //income
+                    incomeSum += item.money
+                } else { //expenses
+                    expensesSum += item.money
+                }
             }
         }
+        //add header and items
+        resultList.add(createHeader(headerDate, incomeSum, expensesSum, tempList.size))
+        resultList.addAll(tempList)
+        //clear item to defualt
+        return@withContext resultList
     }
-    //add header and items
-    resultList.add(createHeader(headerDate, incomeSum, expensesSum, tempList.size))
-    resultList.addAll(tempList)
-    //clear item to defualt
-    return resultList
-}
 
 
 private fun createHeader(
@@ -76,6 +73,19 @@ private fun createHeader(
     }
 }
 
-private fun Long.isTheSameDayAs(other: Long): Boolean =
-    (this.div(DateUtils.DAY_IN_SECONDS)) == other.div(DateUtils.DAY_IN_SECONDS)
+private fun Long.isTheSameDayAs(other: Long): Boolean {
+    //The calendar type (solar hijri or gregorian) does not matter here Used to indicate that
+    // two date are in fact the same day.
+    //GregorianCalendar time zone by default is the device time zone.
+    val thisCalendar = GregorianCalendar().apply {
+        timeInMillis = this@isTheSameDayAs.toMilliSeconds()
+    }
+    val otherCalendar = GregorianCalendar().apply {
+        timeInMillis = other.toMilliSeconds()
+    }
+    return ((thisCalendar.get(Calendar.DATE) == otherCalendar.get(Calendar.DATE)) &&
+            (thisCalendar.get(Calendar.MONTH) == otherCalendar.get(Calendar.MONTH)) &&
+            (thisCalendar.get(Calendar.YEAR) == otherCalendar.get(Calendar.YEAR)))
+
+}
 
